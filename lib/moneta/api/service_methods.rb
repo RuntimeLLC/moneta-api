00e7ff977485d97786777657c237a82eb875670c
ssnikolay <ssnikolay@gmail.com>
2015-04-02 11:31:29 +0300

    fix TODO

--- lib/moneta/api/service_methods.rb
+++ lib/moneta/api/service_methods.rb
@@ -1,14 +1,18 @@
 module Moneta
   module Api
     module ServiceMethods
-      # TODO define for documentation but pretty code for myself ;)
       def find_account_by_id(id)
-        response = client.call(__method__, { message: id })
-        ResponseFactory.build(response)
+        call(__method__, id)
       end
 
       def create_account(request)
-        response = client.call(__method__, { message: request.to_h })
+        call(__method__, request.to_h)
+      end
+
+      private
+
+      def call(method, request_message)
+        response = client.call(method, { message: request_message })
         ResponseFactory.build(response)
       end
     end
