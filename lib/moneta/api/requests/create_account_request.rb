8310b66b3c5711f66e680c452431123c3f8dd0ad
ssnikolay <ssnikolay@gmail.com>
2015-04-03 15:46:36 +0300

    try use multiline comments

--- lib/moneta/api/requests/create_account_request.rb
+++ lib/moneta/api/requests/create_account_request.rb
@@ -4,8 +4,13 @@ module Moneta
       class CreateAccountRequest
         include Moneta::Api::DataMapper
 
+        # @param [String] Валюта счета
+        #   Currency of account
         property :currency
-        property :payment_password
+
+        # @param [String] Название счета. Название счета должно быть уникальным среди счетов одного пользователя. Необязательное поле.
+        #   Alias of account. Must be unique among accounts of given user. Optional.
+        property :alias
       end
     end
   end
