4b0233ba9e67f284e80a1b4f1ffd7275bd078bf3
ssnikolay <ssnikolay@gmail.com>
2015-04-07 10:56:47 +0300

    add transaction types

---
+++ lib/moneta/api/types/entity.rb
@@ -0,0 +1,14 @@
+module Moneta
+  module Api
+    module Types
+      # Базовый тип, содержащий атрибут "version".
+      #   / Base type containing "version" attribute.
+      class Entity
+        include Moneta::Api::DataMapper
+
+        # @return [String]
+        property :version
+      end
+    end
+  end
+end
