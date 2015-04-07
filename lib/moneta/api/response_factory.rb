82427b90a7da98915a76e5f7587b558528634893
ssnikolay <ssnikolay@gmail.com>
2015-04-07 15:12:20 +0300

    refactoring

--- lib/moneta/api/response_factory.rb
+++ lib/moneta/api/response_factory.rb
@@ -14,7 +14,9 @@ module Moneta
           klass = classify(klass)
 
           Object.const_get("Moneta::Api::Responses::#{ klass }").new(
-            map(wsdl.search("//xsd:element[@name='#{ klass }']"), data)
+            data.kind_of?(Hash) ?
+              map(wsdl.search("//xsd:element[@name='#{ klass }']"), data) :
+              data
           )
         end
 
@@ -25,30 +27,23 @@ module Moneta
         end
 
         def map(parent_node, data)
-          if data.kind_of?(Hash)
-            data.keys.count == 1 ?
-              build_element(parent_node, *data.to_a.first) :
-              data.map { |key, value| build_element(parent_node, key, value) }.reduce(Hash.new, :merge)
-          else
-            data
-          end
+          data.keys.count == 1 ?
+            build_element(parent_node, *data.to_a.first) :
+            data.map { |key, value| build_element(parent_node, key, value) }.reduce(Hash.new, :merge)
         end
 
         def build_element(node, key, value)
-          if value.kind_of?(Hash)
-            build_node(node, key, value, Proc.new { |child_node| map(child_node, value) }, Proc.new { raise 'error' })
-          else
-            build_node(node, key, value, Proc.new { |child_node| child_node }, Proc.new { { key => value} })
-          end
-        end
-
-        def build_node(node, key, value, block1, block2)
           child_node = node.search("[@name='#{ key }']")
           type = child_node.any? && child_node.attribute('type').value
-          if TYPES_MAP.has_key?(type)
-            { key => TYPES_MAP[ type ].new(block1.call(child_node, value)) }
+
+          if value.kind_of?(Hash)
+            if TYPES_MAP.has_key?(type)
+              { key => TYPES_MAP[ type ].new(map(child_node, value)) }
+            else
+              Proc.new { raise 'error' }
+            end
           else
-            block2.call
+            { key => value}
           end
         end
       end
