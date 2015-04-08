f87d75231fff488a57a020dd30fc033e558b276b
ssnikolay <ssnikolay@gmail.com>
2015-04-08 14:53:32 +0300

    remove tralling spaces

--- lib/moneta/api/data_mapper.rb
+++ lib/moneta/api/data_mapper.rb
@@ -8,7 +8,7 @@ module Moneta
           def fill(data)
             properties.each do |property, type|
               value = data[ property ]
-              
+
               if value
                 property_value = type.nil? ? value : build_complex_value(type, value)
 
