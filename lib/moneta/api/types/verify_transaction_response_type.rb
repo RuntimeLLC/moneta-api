a158aa497f9407705d7a82a03a425137f8d12656
ssnikolay <ssnikolay@gmail.com>
2015-04-13 14:35:29 +0300

    Moneta::Api::Service#verify_transaction

---
+++ lib/moneta/api/types/verify_transaction_response_type.rb
@@ -0,0 +1,18 @@
+module Moneta
+  module Api
+    module Types
+      # Ответ на запрос проверки проведения операции в системе МОНЕТА.РУ
+      # Transaction verification response.
+
+      class VerifyTransactionResponseType < VerifyTransferResponseType
+        # @return [Integer] Идентификатор операции в системе МОНЕТА.РУ.
+        #   / The identifier of transaction.
+        property :transaction_id
+
+        # @return [String] Статус операции в системе МОНЕТА.РУ.
+        #   / Transaction status type in MONETA.RU
+        property :operation_status
+      end
+    end
+  end
+end
