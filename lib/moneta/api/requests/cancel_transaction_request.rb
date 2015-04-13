fd15433661b4bd602a6cb8cd7bbf83c03fe89bc7
ssnikolay <ssnikolay@gmail.com>
2015-04-13 19:03:57 +0300

    Moneta::Api::Service#cancel_transaction

---
+++ lib/moneta/api/requests/cancel_transaction_request.rb
@@ -0,0 +1,14 @@
+module Moneta
+  module Api
+    module Request
+      # Запрос на отмену операции. Отменять операцию можно при выполнении всех условий:
+      # незавершенные операции;
+      # если есть доступ к счету плательщика и счету получателя (то есть свои внутренние операции).
+      # Transaction cancelation request if transasction fulfils following conditions:
+      # transaction is in processing (incompleted);
+      # principal has an access to both (payer and payee) accounts.
+
+      class CancelTransactionRequest < Types::CancelTransactionRequestType; end
+    end
+  end
+end
