c602361129896cbcb14d6a08b913911e52bc5b37
ssnikolay <ssnikolay@gmail.com>
2015-10-12 15:57:47 +0300

    try fix require files

--- lib/moneta/api.rb
+++ lib/moneta/api.rb
@@ -7,7 +7,7 @@ require 'moneta/api/service'
 require 'moneta/api/data_mapper'
 
 %w(types responses requests).each do |folder|
-  Dir["lib/moneta/api/#{ folder }/*.rb"].each do |file|
+  Dir["./lib/moneta/api/#{ folder }/*.rb"].each do |file|
     require File.join(File.dirname(__FILE__), '../../', file)
   end
 end
