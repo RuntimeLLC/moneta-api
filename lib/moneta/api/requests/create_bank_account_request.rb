f5383ab530e5e6bfd1d4f14b2a53b931ff80de4a
Александр Фомин <ll.wg.bin@gmail.com>
2015-10-21 12:23:24 +0300

    fix create bank account

--- lib/moneta/api/requests/create_bank_account_request.rb
+++ lib/moneta/api/requests/create_bank_account_request.rb
@@ -11,17 +11,17 @@ module Moneta
         #   / Bank account ID.
         property :id
 
-        # @return [Integer] Пользователь, которому будут принадлежать банковские реквизиты.
-        #   / Если это поле не задано, то банковские реквизиты создаются для текущего пользователя.
-        #   / The unique identifier of the user who will own the bank account.
-        #   / If you omit this element, MONETA.RU uses the ID of the user who sends the request.
-        property :unit_id
-
         property :profile_id
 
         # @return [Moneta::Api::Types::BankAccountType] Расширенные атрибуты.
         #   / Specifies extended attributes.
         property :attribute, type: Moneta::Api::Types::BankAccountType
+
+        # @return [Integer] Пользователь, которому будут принадлежать банковские реквизиты.
+        #   / Если это поле не задано, то банковские реквизиты создаются для текущего пользователя.
+        #   / The unique identifier of the user who will own the bank account.
+        #   / If you omit this element, MONETA.RU uses the ID of the user who sends the request.
+        property :unit_id
       end
     end
   end
