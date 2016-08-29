ae03ea926cd1630995a028a429894d1fc0d86044
Александр Фомин <ll.wg.bin@gmail.com>
2016-08-29 14:23:49 +0300

    withdrawal constants

---
+++ lib/moneta/constants/withdrawal.rb
@@ -0,0 +1,14 @@
+module Moneta
+  module Constants
+    module Withdrawal
+      BANK = 5
+      YANDEX_MONEY = 13
+      VKONTAKTE = 139
+      CYBER_PLAT = 1144
+      CARD_RUSSIAN_STANDARD = 275
+      CARD_ALFA_BANK = 279
+      MAIL_MONEY = 268
+      QIWI = 255
+    end
+  end
+end
